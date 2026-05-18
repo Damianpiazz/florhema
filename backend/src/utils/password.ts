d234661b165a6b652f37384cc5658b571778b543
Damianpiazz <damipiazza333@gmail.com>
2026-05-18 00:44:27 -0300

    feat(backend): agregar funcion de hashing de contrasena

---
+++ backend/src/utils/password.ts
@@ -0,0 +1,6 @@
+import bcrypt from 'bcrypt'
+import { AUTH } from '../config/auth'
+
+export async function hashPassword(password: string): Promise<string> {
+  return bcrypt.hash(password, AUTH.SALT_ROUNDS)
+}
