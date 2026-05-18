72fa916177b3d5d5aed0cfdd878e7471cd4562d5
Damianpiazz <damipiazza333@gmail.com>
2026-05-17 22:53:19 -0300

    feat(backend): configurar app con cors, json, rutas y error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,16 @@
 import 'dotenv/config'
-
 import express, { type Application } from 'express'
+import cors from 'cors'
+import corsConfig from '@/config/cors'
+import apiRoutes from '@/routes/index'
+import { setupSwagger } from '@/config/swagger'
+import { errorHandler } from '@/middlewares/error-handler'
 
 const app: Application = express()
+
+app.use(cors(corsConfig))
+
+app.use(express.json())
 
 // HEALTHCHECK
 app.get('/health', (_req, res) => {
@@ -12,5 +20,11 @@ app.get('/health', (_req, res) => {
     timestamp: new Date().toISOString()
   })
 })
+
+setupSwagger(app)
+
+app.use('/api/v1', apiRoutes)
+
+app.use(errorHandler)
 
 export default app
